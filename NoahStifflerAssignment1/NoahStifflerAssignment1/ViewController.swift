adb3524edb22aa2fd6af49eabd58ec1d38e20085
Noah Stiffler <stifflernoah@gmail.com>
2020-02-03 21:51:16 -0500

    Add files via upload

---
+++ NoahStifflerAssignment1/NoahStifflerAssignment1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoahStifflerAssignment1
+//
+//  Created by Noah Stiffler on 2/3/20.
+//  Copyright © 2020 Noah Stiffler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
